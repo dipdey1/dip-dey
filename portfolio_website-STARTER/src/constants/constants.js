6ecd47ae75d7ade0a0c9dc7fc2d11c23399bece8
Dip Dey <100687361+dipdey1@users.noreply.github.com>
2022-12-21 11:34:30 +0530

    update constant.js

--- portfolio_website-STARTER/src/constants/constants.js
+++ portfolio_website-STARTER/src/constants/constants.js
@@ -49,7 +49,7 @@ export const projects = [
 export const TimeLineData = [
   { year: 2019, text: 'Started my journey', },
   { year: 2020, text: 'Started as a Program Associate at Vedantu', },
-  { year: 2022, text: 'Worked as a freelance developer alongside.', },
   { year: 2021, text: 'Started building projects for Center of Excellence.', },
+  { year: 2022, text: 'Worked as a freelance developer alongside.', },
   { year: 2023, text: 'Planning to create my own platform that can host a million users', },
 ];
